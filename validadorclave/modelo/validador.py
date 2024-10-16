f296fa3a7c82f35c00ecf458cb5ed82f26d9cdb2
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2024-10-16 12:52:02 -0500

    creation class ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,14 @@
 # TODO: Implementa el código del ejercicio aquí
+
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion(ABC):
+
+    def _init_(self, longitud_esperada: int):
+        self._longitud_esperada: int = longitud_esperada
+
+
+
+
+
