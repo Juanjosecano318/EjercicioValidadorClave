89197ae37e821c3a20b7203f2b5735f457bc8281
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2024-10-16 12:53:09 -0500

    creation method abstract

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -8,6 +8,32 @@ class ReglaValidacion(ABC):
     def _init_(self, longitud_esperada: int):
         self._longitud_esperada: int = longitud_esperada
 
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        """Método abstracto que debe ser implementado por las subclases para verificar la validez de la clave"""
+        pass
+
+    def _validar_longitud(self, clave: str) -> bool:
+        return len(clave) > self._longitud_esperada
+
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        for c in clave:
+            if c.isupper():
+                return True
+        return False
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        for c in clave:
+            if c.islower():
+                return True
+        return False
+
+    def _contiene_numero(self, clave: str) -> bool:
+        for c in clave:
+            if c.isdigit():
+                return True
+        return False
 
 
 
